8155cb5138d0548d2748345ce1881819a2f76a64
JashanDeep Singh <deepsainii2508@gmail.com>
2023-04-13 21:06:04 -0400

    Initial Commit

---
+++ src/main/java/com/cpan282/distributionCenter/DistributionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.cpan282.distributionCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributionCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpan282/distributionCenter/DistributionCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpan282.distributionCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
